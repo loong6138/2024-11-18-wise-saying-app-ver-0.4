416196775f9f776032436f108695180e63489ac7
loong6138 <loong6138@gmail.com>
2024-11-22 17:40:02 +0900

    step 11

---
+++ src/main/java/com/ll/wiseSaying/app/App.java
@@ -0,0 +1,19 @@
+package com.ll.wiseSaying.app;
+
+import com.ll.wiseSaying.controller.WiseSayingController;
+
+import java.util.Scanner;
+
+public class App {
+
+
+    public void run() {
+
+        WiseSayingController controller = new WiseSayingController();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+
+
+    }
+}
